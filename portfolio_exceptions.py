eb98600fb82f8ec97cfefcc6006b6b43272884c3
AlinaD-2912 <dvornyk.alina2912@gmail.com>
2025-09-10 16:33:09 +0200

    step 5 advancing

---
+++ portfolio_exceptions.py
@@ -0,0 +1,28 @@
+from portfolio_structures import *;
+from typing import List
+from collections import namedtuple
+
+positions_problematiques = [
+    Position('AAPL', 10, 0.0, '2023-01-15'),      # Prix d'achat = 0 !
+    Position('INVALID', 5, 100.0, '2023-02-01'),  # Symbole inexistant
+    Position('GOOGL', -10, 2500.0, '2023-03-01')  # Quantité négative !
+]
+
+def ErreurDonneesPortfolio (portfolio: Portfolio, position: Position):
+    try:
+        if position.purchase_price <= 0 :
+            raise ErreurDonneesPortfolio("Prix d'achat invalide pour {position.symbol} : {position.purchase_price}")
+        if position.quantity <=0 :
+            raise  ErreurDonneesPortfolio("Quantité invalide pour {position.symbol} : {position.quantity}")
+        if not position.symbol.isAlpha():
+            raise ErreurDonneesPortfolio("Symbol invalide : {position.symbol} ")
+        portfolio.add_position(position)
+    except ErreurDonneesPortfolio as e:
+        print("Erreur détecté: {e}")
+    except Exception as e:
+        print(f"Erreur imprévue est survenue: {e}")
+
+portfolio = Portfolio("Test")
+for pos in positions_problematiques:
+    portfolio.add_position(pos)
+
